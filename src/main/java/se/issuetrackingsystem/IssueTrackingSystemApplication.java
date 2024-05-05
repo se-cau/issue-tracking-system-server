611a20e2fe690f31cf45f87bfbcf1c4a2f962ac0
2522001 <minseo770@gmail.com>
2024-05-05 22:58:15 +0900

    init: #1 - 프로젝트 생성

---
+++ src/main/java/se/issuetrackingsystem/IssueTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package se.issuetrackingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackingSystemApplication.class, args);
+	}
+
+}
