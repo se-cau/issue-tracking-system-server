5ca652eb942729dbbff3716b22966263b169ee91
2522001 <minseo770@gmail.com>
2024-05-05 23:29:33 +0900

    init: #1 - ExceptionHandler 설정

--- src/main/java/se/issuetrackingsystem/exception/CustomException.java
+++ src/main/java/se/issuetrackingsystem/exception/CustomException.java
@@ -1,2 +1,10 @@
-package se.issuetrackingsystem.exception;public class CustomException {
+package se.issuetrackingsystem.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
 }
