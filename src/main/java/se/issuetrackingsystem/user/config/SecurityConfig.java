4352d88ff62fb35f41446d0b8e296fbe3d85e5c6
2522001 <minseo770@gmail.com>
2024-05-14 15:13:57 +0900

    feat: #3 - h2 데이터베이스 접근 설정

--- src/main/java/se/issuetrackingsystem/user/config/SecurityConfig.java
+++ src/main/java/se/issuetrackingsystem/user/config/SecurityConfig.java
@@ -1,2 +1,26 @@
-package se.issuetrackingsystem.user.config;public class SecurityConfig {
+package se.issuetrackingsystem.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+        .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+            .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+        .csrf((csrf) -> csrf
+                .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+        .headers((headers) -> headers
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));;
+
+        return http.build();
+    }
 }
