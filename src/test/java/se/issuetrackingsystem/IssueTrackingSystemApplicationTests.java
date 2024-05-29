765b42d64734470298f10d9ec021bfa8b335e592
Feel5nL\Feel5nL <dksvlfdhs1@naver.com>
2024-05-29 17:24:59 +0900

    fix: IssueServiceTests 수정

--- src/test/java/se/issuetrackingsystem/IssueTrackingSystemApplicationTests.java
+++ src/test/java/se/issuetrackingsystem/IssueTrackingSystemApplicationTests.java
@@ -1,9 +1,14 @@
 package se.issuetrackingsystem;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.location=" +
+        "classpath:/application.yml")
 class IssueTrackingSystemApplicationTests {
+    @Test
+    void contextLoads() {
+    }
 
 }
