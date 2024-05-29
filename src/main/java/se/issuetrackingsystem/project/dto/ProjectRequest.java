be9d3bbdf164d4597347d8a12ae4137553e4b28d
2522001 <minseo770@gmail.com>
2024-05-29 17:48:37 +0900

    fix: #26 - 불필요한 코드 삭제
    @NotBlank는 문자열 타입에만 유효하다.

--- src/main/java/se/issuetrackingsystem/project/dto/ProjectRequest.java
+++ src/main/java/se/issuetrackingsystem/project/dto/ProjectRequest.java
@@ -13,9 +13,7 @@ public class ProjectRequest {
     @NotBlank(message = "Title은 공백이 될 수 없습니다.")
     private final String title;
 
-    @NotBlank(message = "Admin Id는 공백이 될 수 없습니다.")
     private final Long adminId;
 
     private final List<Long> contributorIds;
-
 }
