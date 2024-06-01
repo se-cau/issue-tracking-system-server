433e08751c6e54cc76929245c5517c4301b308a1
2522001 <minseo770@gmail.com>
2024-06-01 19:06:12 +0900

    fix: cors 에러 해결

--- src/main/java/se/issuetrackingsystem/common/config/WebConfig.java
+++ src/main/java/se/issuetrackingsystem/common/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "http://ec2-3-39-232-218.ap-northeast-2.compute.amazonaws.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
